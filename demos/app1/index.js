40817eed7eef6d338be0a3c16b47f97999ce84ef
Han Han <haannn@qq.com>
2023-12-01 05:25:35 +0000

    feat: init

---
+++ demos/app1/index.js
@@ -0,0 +1,7 @@
+import { lib2 } from "@internel/lib2"
+
+export function app1() {
+    lib2()
+    console.log('hello app1')
+}
+app1()
---
+++ demos/lib2/index.js
@@ -0,0 +1,7 @@
+import { lib1 } from "@internel/lib1"
+
+export function lib2() {
+    lib1()
+    console.log('hello lib2')
+
+}
